9ff18fc9fefea1d81c193b5c089518f310d8542d
Yvonne Kanvu <yvkanvu@gmail.com>
2024-04-25 12:59:24 +0200

    création de la page Contact

---
+++ src/Component/NosContact.jsx
@@ -0,0 +1,10 @@
+function NosContact() {
+    return ( 
+        <di>
+            <p>+243 820356185</p>
+            <p>yvkanvu@gmail.com</p>
+        </di>
+     );
+}
+
+export default NosContact;
---
+++ src/Page/Contact.jsx
@@ -0,0 +1,9 @@
+import NosContact from "../Component/NosContact";
+
+function Contact() {
+    return ( 
+        <NosContact/>
+     );
+}
+
+export default Contact;
